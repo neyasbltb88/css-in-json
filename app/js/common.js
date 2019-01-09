5dc12d610cf193f85a0359252fef871a09069852
Neyasbltb <neyasbltb_88@mail.ru>
2019-01-09 18:15:29 +0300

    Добавил prism.js для подсветки синтаксиса на демо-странице

--- app/js/common.js
+++ app/js/common.js
@@ -1,30 +1,60 @@
 window.style = {
     'body': {
-        'margin': 0,
-        'background-color': '#333',
+        'margin': '30px 0',
+        'background-color': '#555',
     },
 
     '.container': {
         margin: '0 auto',
         backgroundColor: '#dedede',
+        boxShadow: '0 0 20px 0px rgba(0, 0, 0, 0.5)',
         maxWidth: '800px',
         padding: '15px',
+        borderRadius: '3px',
+    },
+
+    '.inline': {
+        display: 'inline-block',
+        margin: 0,
+        padding: 0,
     },
 
     'h2, h3': {
         textAlign: 'center'
     },
 
+    '.rich_h3': {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
     '.section': {
-        border: '1px solid #000',
         marginBottom: '15px',
         padding: '15px',
-        borderRadius: '5px',
     },
 
     '.section:last-child': {
         marginBottom: 0,
-    }
+    },
+
+    '.pre_block': {
+        border: '1px solid #bbb',
+        borderRadius: '3px',
+        padding: '1em',
+        margin: '.5em 0',
+        overflow: 'auto',
+        boxShadow: '0 0 8px 0px rgba(0, 0, 0, 0.1)',
+    },
+
+    'pre.inline': {
+        display: 'inline-block',
+        padding: '0 5px',
+    },
+
+    '.language-json .token.operator, .language-javascript .token.operator': {
+        background: 'transparent',
+    },
 }
 
 window.style2 = {
@@ -38,9 +68,10 @@ window.style2 = {
 
 //////////////////////////////////////
 
-window.CssInJson = new CSSinJSON({
+CssInJson = new CSSinJSON({
     elem: 'body',
     // elem: '.container',
+    // elem: '.section_2',
     style: style,
     scoped: true
 });
@@ -54,7 +85,7 @@ let style_show = document.querySelector('.style_show');
 let json_show = document.querySelector('.json_show');
 let base_selector = document.querySelector('.base_selector')
 
-style_info.textContent = `В head вставлен тег <style id="${CSSinJSON_style.id}" class="${CSSinJSON_style.className}"></style>`;
+style_info.textContent = `<style id="${CSSinJSON_style.id}" class="${CSSinJSON_style.className}"></style>`;
 style_show.textContent = CSSinJSON_style.textContent;
 
 json_show.textContent = JSON.stringify(window.style, ' ', 4);
